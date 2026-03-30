3ae60ec34868db0ff78c1f4e9226d5ed18525a13
Lucca de Oliveira <17157044@hemominas.mg.gov.br>
2026-03-30 19:10:35 -0300

    Automatizador v2: Att: cria JSON, verifica se existe resultados.

--- a.py
+++
@@ -1,5 +0,0 @@
-contador = 1
-
-for x in range(5):
-    print(contador)
-    contador = contador + 1
--- reboot_test.py
+++ reboot_test.py
@@ -1,13 +1,72 @@
-import asyncio
+import asyncio, os, base64, json
 from playwright.async_api import async_playwright
 from playwright_stealth import Stealth
 
 async def cookie(page):
     try:
-            await page.click("#accept-all-btn", timeout=3000)
+            await page.click("#accept-all-btn", timeout=500)
             print("Cookies aceitos")
     except:
-            print("Banner de cookies não apareceu, seguindo...")
+            pass
+
+
+async def esperar_verificacao_humana(page):
+    try:
+        await page.wait_for_selector("text=Let's confirm you are human", timeout=15000)
+        print("⚠️ Verificação detectada. Resolve no navegador...")
+        
+        await page.wait_for_function(
+            "() => !window.location.href.includes('challenges.cloudflare.com')",
+            timeout=180000
+        )
+        print("Seguindo...")
+    except:
+        pass
+
+async def salva_base(dados_user):
+     with open(f"arquivos/imagens/{dados_user}.png", "rb") as f:
+          bytes_img = f.read()
+          encoded = base64.b64encode(bytes_img)
+          str_base = encoded.decode("utf-8")
+          
+          print(str_base[:100])
+          return str_base, dados_user
+
+async def salva_json(str_base, nome, cpf, nis):
+
+    dados = {
+          "nome" : nome,
+          "cpf" : cpf,
+          "nis" : nis,
+          "screenshot" : str_base
+            }
+    json_string = json.dumps(dados, ensure_ascii= False, indent = 2)
+    print(json_string)
+
+    with open(f"arquivos/{nome}.json", "w", encoding="utf-8") as f:
+         json.dump(dados, f, ensure_ascii = False, indent = 2)
+
+
+async def captura_dados(page):
+    try:
+        nome = await page.locator("section.dados-tabelados div.col-xs-12.col-sm-4:has(strong:text('Nome')) span").inner_text(timeout=3000)
+        nome = nome.strip()
+    except:
+        nome = None
+
+    try:
+        cpf = await page.locator("section.dados-tabelados div.col-xs-12.col-sm-3:has(strong:text('CPF')) span").inner_text(timeout=3000)
+        cpf = cpf.strip()
+    except:
+        cpf = None
+
+    try:
+        nis = await page.locator("#tabela-visao-geral-sancoes tbody tr:first-child td:nth-child(2)").inner_text(timeout=3000)
+        nis = nis.strip()
+    except:
+        nis = None
+
+    return nome, cpf, nis
 
 
 async def run():
@@ -15,68 +74,95 @@ async def run():
     async with async_playwright() as p:
 
         #Abrindo o navegador
-        browser = await p.firefox.launch(headless=False)
+        browser = await p.chromium.launch(headless=False, channel="chrome")
 
+        if os.path.exists("sessao.json"):
+            print("Sessão encontrada, carregando...")
+            context = await browser.new_context(
+                viewport={"width": 1366, "height": 768},
+                locale="pt-BR",
+                timezone_id="America/Sao_Paulo",
+                storage_state="sessao.json"
+            )
+        else:
+            print("Sem sessão salva, abrindo do zero...")
+            context = await browser.new_context(
+                viewport={"width": 1366, "height": 768},
+                locale="pt-BR",
+                timezone_id="America/Sao_Paulo"
+            )
         #Abre uma nova pagina sem cache
-        context = await browser.new_context(
-    user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36"
-)
         page = await context.new_page()
-        await Stealth().apply_stealth_async(page) #Mascara o script pra não cair em captcha
+        #Mascara o script pra não cair em captcha
+        
         
         #vai até a pagina
         await page.goto("https://portaldatransparencia.gov.br/pessoa/visao-geral")
-        await page.wait_for_timeout(3000)
+        await page.wait_for_timeout(500)
         await page.wait_for_load_state("networkidle")
         
 
         #clica para consultar Pessoa Fisica
-        await page.wait_for_timeout(3000)
+        await page.wait_for_timeout(500)
         await cookie(page)
-        await page.wait_for_timeout(3000)
+        await page.wait_for_timeout(500)
         await page.click("#button-consulta-pessoa-fisica")
-        await page.wait_for_timeout(3000)
+        await page.wait_for_timeout(500)
         await page.wait_for_load_state("networkidle")
-        await page.wait_for_timeout(3000)
+        await page.wait_for_timeout(500)
 
 
+        while True:
+                pesquisa = input("Digite o termo a ser buscado: ")
+                await page.get_by_placeholder("Busque por Nome, Nis ou CPF (123456789-00)").fill(pesquisa)
+                await page.wait_for_timeout(500)
+                await page.click("button[aria-label='Enviar dados do formulário de busca']")
+                await page.wait_for_selector("#countResultados")
+                await page.wait_for_timeout(500)
+                resultado = await page.locator("p:has(#countResultados)").inner_text()
+                print(resultado)
+                await page.wait_for_load_state("networkidle")
 
-        #preenche o campo corretamento:
-        await cookie(page)
-        await page.wait_for_timeout(3000)
-        
-        #pesquisa:
-        pesquisa = "Maria da Silva"
-        await page.get_by_placeholder("Busque por Nome, Nis ou CPF (123456789-00)").fill(pesquisa)
-        await page.wait_for_timeout(3000)
-        await page.click("button[aria-label='Enviar dados do formulário de busca']")
-        await page.wait_for_selector("#countResultados")
-        await page.wait_for_timeout(3000)
-        resultado = await page.locator("p:has(#countResultados)").inner_text()
-        print(resultado)
+                if "0 resultados" not in resultado.lower():
+                        break  # achou — sai do loop
 
-        #clicar no primeiro:
+                print("Sem resultados, tente novamente.")
 
+
+                    #clicar no primeiro:
+        await page.wait_for_load_state("networkidle")
         primeiro = page.locator("#resultados a.link-busca-nome").first
         await primeiro.click()
-        await page.wait_for_timeout(3000)
+        await page.wait_for_timeout(500)
 
         await cookie(page)
         await page.wait_for_load_state("networkidle")
-        await page.wait_for_timeout(3000)
+        await page.wait_for_timeout(500)
 
-        #Abre a tabela:
+                #Abre a tabela:
         tabela = page.locator("button[aria-controls='accordion-recebimentos-recursos']")
         await tabela.click()
-        await page.wait_for_timeout(3000)
+        await page.wait_for_timeout(500)
+        nome, cpf, nis = await captura_dados(page)
+        await page.screenshot(path=f"arquivos/imagens/{nome}.png", full_page=True)
+        str_base, _ = await salva_base(nome)
+        await salva_json(str_base, nome, cpf, nis)
+
         detalhe = page.locator("a:has-text('Detalhar')")
         await detalhe.click()
-        await page.wait_for_timeout(3000)
+        await esperar_verificacao_humana(page)
+        await page.wait_for_timeout(500)
         await page.wait_for_load_state("networkidle")
-        await page.wait_for_timeout(3000)
-        await page.screenshot(path="aaaaaa.png", full_page=True)
-        #fecha o navegador
+        await page.wait_for_timeout(500)
+        await page.screenshot(path=f"{nome}.png", full_page=True)
+        await salva_base(nome)
+
+
+                        
+        await context.storage_state(path="sessao.json")
+                #fecha o navegador
         await browser.close()
 
-    
+
+
 asyncio.run(run())
